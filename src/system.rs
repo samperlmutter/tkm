d44500703e775285e7f4deeaa27abf187026a519
= <sam.perlmutter227@gmail.com>
2019-05-04 23:15:11 -0400

    Cleaned up System struct

--- src/system.rs
+++ src/system.rs
@@ -3,7 +3,7 @@ use sysinfo::{SystemExt, ProcessorExt};
 use crate::process::Process;
 
 pub struct System {
-    system: sysinfo::System,
+    sysinfo: sysinfo::System,
     pub cpu_usage_history: Vec<u64>,
     pub cpu_current_usage: u64,
     pub cpu_num_cores: usize,
@@ -17,20 +17,20 @@ pub struct System {
 
 impl System {
     pub fn new(initial_size: u16) -> System {
-        let system = sysinfo::System::new();
+        let sysinfo = sysinfo::System::new();
 
         let history_width = initial_size / 2;
 
         // Overall CPU usage
         let cpu_usage_history = vec![0; history_width as usize];
-        let cpu_num_cores: usize = system.get_processor_list().len() - 1;
+        let cpu_num_cores: usize = sysinfo.get_processor_list().len() - 1;
 
         // Memory usage
-        let mem_total = system.get_total_memory();
+        let mem_total = sysinfo.get_total_memory();
         let mem_usage_history = vec![0; history_width as usize];
 
         System {
-            system,
+            sysinfo,
             cpu_usage_history,
             cpu_current_usage: 0,
             cpu_num_cores,
@@ -44,28 +44,28 @@ impl System {
     }
 
     pub fn update(&mut self) -> System {
-        self.system.refresh_all();
+        self.sysinfo.refresh_all();
 
         // Overall CPU usage
-        self.cpu_current_usage = (self.system.get_processor_list()[0].get_cpu_usage() * 100.0).round() as u64;
+        self.cpu_current_usage = (self.sysinfo.get_processor_list()[0].get_cpu_usage() * 100.0).round() as u64;
         self.cpu_usage_history.push(self.cpu_current_usage);
         self.cpu_usage_history.remove(0);
 
         // Memory usage
-        self.mem_used = self.system.get_used_memory();
-        self.mem_free = self.system.get_free_memory();
+        self.mem_used = self.sysinfo.get_used_memory();
+        self.mem_free = self.sysinfo.get_free_memory();
         self.mem_usage_history.push(self.mem_used);
         self.mem_usage_history.remove(0);
 
         // CPU core usage
-        self.cpu_core_usages = self.system.get_processor_list()
+        self.cpu_core_usages = self.sysinfo.get_processor_list()
             .iter()
             .skip(1)
             .map(|p| (p.get_cpu_usage() * 100.0).round() as u16)
             .collect();
 
         // Processes
-        self.processes = self.system.get_process_list()
+        self.processes = self.sysinfo.get_process_list()
             .iter()
             .map(|(_, process)|
                 Process::new(process)
@@ -73,16 +73,12 @@ impl System {
             .collect();
 
         System {
-            system: sysinfo::System::new(),
+            sysinfo: sysinfo::System::new(),
             cpu_usage_history: self.cpu_usage_history.clone(),
-            cpu_current_usage: self.cpu_current_usage,
-            cpu_num_cores: self.cpu_num_cores,
-            mem_total: self.mem_total,
-            mem_free: self.mem_free,
-            mem_used: self.mem_used,
             mem_usage_history: self.mem_usage_history.clone(),
             cpu_core_usages: self.cpu_core_usages.clone(),
-            processes: self.processes.clone()
+            processes: self.processes.clone(),
+            ..*self
         }
     }
 }
